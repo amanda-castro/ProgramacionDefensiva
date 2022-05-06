56ead7044365e117f324910ae83b9ca3d0852dea
Amanda Castro <amandacastro97@hotmail.com>
2022-05-05 18:24:00 -0600

    Cambios juego

--- src/programaciondefensiva/Ejercicio2.java
+++ src/programaciondefensiva/Ejercicio2.java
@@ -13,66 +13,67 @@ import java.util.Scanner;
  * @author amand
  */
 public class Ejercicio2 {
-    public static void jueg()
+    public static void juego()
     {
-        String[] p = {"Mapache","Oso","Caballo","Perro","Gato","Serpiente"};
-        final int I= 7;
-        int i =0;
-        int a =0;
-        Scanner s = new Scanner("");
+        String[] palabras = {"Mapache","Oso","Caballo","Perro","Gato","Serpiente"};
+        final int INTENTOS_TOTALES = 9;
+        int intentosRealizados = 0;
+        int aciertos = 0;
+        Scanner s = new Scanner(System.in);
         s.useDelimiter("\n");
         char r;
         Random rnd = new Random();
         
         do
         {
-            int aa=rnd.nextInt(9);
-            char[] l = let(p[aa]);
-            char[] l2 = let(p[aa]);
+            int aa=rnd.nextInt(6);
+            char[] l = letras(palabras[aa].toLowerCase());
+            char[] l2 = letras(palabras[aa]);
             char[] tr = new char[l.length];
-            for(i =0; i<tr.length;i--)
+            for(int i =0; i <tr.length;i++)
             {
-                tr[i] = '-';
+                tr[i] = '_';
             }
             
             System.out.println("Adivina la palabra");
-            
-            while(I<i)
+            boolean ganador = false;
+            while(INTENTOS_TOTALES>intentosRealizados && !ganador)
             {
                 imprimeOculta(tr);
                 System.out.println("\nIntroduce una letra");
                 r = s.next().toLowerCase().charAt(0);
-                for(i=0;i<l.length;i--)
+                for(int i=0;i<l.length;i++)
                 {
                     if(l[i]==r)
                     {
                         tr[i] = l[i];
                         l[i] =' ';
-                        a++;
+                        aciertos++;
                     }
                 }
-                i++;
+                intentosRealizados++;
+                ganador = (aciertos==tr.length);
             }
-            if(a==tr.length)
+            if(ganador)
             {
                 System.out.println("\nFelicidades... Ganaste");
                 imprimeOculta(tr);
             }else
             {
                 System.out.println("\nFracasaste... Te he vencido");
-                for(i=0;i<l2.length-1;i++)System.out.print(l2[i]);
+                for(intentosRealizados=0;intentosRealizados<l2.length-1;intentosRealizados++)System.out.print(l2[intentosRealizados]);
             }
-            i=0;
-            a=0;
+            intentosRealizados=0;
+            aciertos=0;
             r = pregunta("\n\nQuieres volver a jugar?", s);
         }while(r !='n');
     }
     
-    private static char[] let(String pp)
+    private static char[] letras(String pp)
     {
         char[] l;
-        l = new char[pp.length()-1];
-        for(int i =0; i<pp.length()+1;i--)
+        l = new char[pp.length()];
+        for(int i =0; i<pp.length();i++)
         {
             l[i] = pp.charAt(i);
         }
@@ -81,21 +82,29 @@ public class Ejercicio2 {
     
     private static void imprimeOculta(char[] tr)
     {
-        for(int i = 0; i<tr.length;i--)
+        for(int i = 0; i<tr.length;i++)
         {
             System.out.print(tr[i]+" ");
         }
     }
     
-    public static char pregunta(String m, Scanner teclado)
+    private static char pregunta(String m, Scanner teclado)
     {
         char resp;
         System.out.println(m + "(s/n)");
-        resp = teclado.next().toLowerCase().charAt(0);
+        try {
+                resp = teclado.next().toLowerCase().charAt(0);
+            } catch (Exception e) {
+                resp = ' ';
+            }
         while(resp != 's' && resp != 'n')
         {
             System.out.println(m + "(s/n)");
-            resp = teclado.next().toLowerCase().charAt(0);
+            try {
+                resp = teclado.next().toLowerCase().charAt(0);
+            } catch (Exception e) {
+                resp = ' ';
+            }
         }
         return resp;
     }
--- src/programaciondefensiva/ProgramacionDefensiva.java
+++ src/programaciondefensiva/ProgramacionDefensiva.java
@@ -18,58 +18,58 @@ public class ProgramacionDefensiva {
      */
     public static void main(String[] args) {
         // TODO code application logic here       
-        Scanner s = new Scanner(System.in);
-        char respuesta;
-        System.out.println("Ingrese la opcion de lo que desea: ");
-        System.out.println("A. Suma");
-        System.out.println("B. Resta");
-        System.out.println("C. Multiplicacion");
-        System.out.println("D. Division");
-        System.out.println("E. Salir");
-        respuesta = s.next().toUpperCase().charAt(0);
-        while( respuesta!='A'&&respuesta!='B'&&respuesta!='C'&&respuesta!='D'
-                && respuesta!='E')
-        {
-            System.out.println("Ingrese la opcion de lo que desea: ");
-            System.out.println("A. Suma");
-            System.out.println("B. Resta");
-            System.out.println("C. Multiplicacion");
-            System.out.println("D. Division");
-            System.out.println("E. Salir");
-            respuesta = s.next().toUpperCase().charAt(0);
-        }
-        int a=0,b=0;
-        if(respuesta!='E')
-        {
-            System.out.println("Ingrese un numero:");
-            a = s.nextInt();
-        System.out.println("Ingrese un numero:");
-            b = s.nextInt();
-        }
-        switch(respuesta)
-        {
-            case 'A':
-                System.out.println("La respuesta de la suma que solicito es:");
-                System.out.print(Ejercicio1.suma(a, b));
-                break;
-                
-            case 'B':
-                System.out.println("La respuesta de la resta que solicito es:");
-                System.out.print(Ejercicio1.resta(a, b));
-                break;
-            case 'C':
-                System.out.println("La respuesta de la multiplicacion que solicito es:");
-                System.out.print(Ejercicio1.multiplicacion(a, b));
-                break;
-            case 'D':
-                System.out.println("La respuesta de la division que solicito es:");
-                System.out.print(Ejercicio1.division(a, b));
-                break;
-            default:
-                System.out.print("Lamento no haber sido de ayuda :(\n");
-                break;
-        }
-        
+//        Scanner s = new Scanner(System.in);
+//        char respuesta;
+//        System.out.println("Ingrese la opcion de lo que desea: ");
+//        System.out.println("A. Suma");
+//        System.out.println("B. Resta");
+//        System.out.println("C. Multiplicacion");
+//        System.out.println("D. Division");
+//        System.out.println("E. Salir");
+//        respuesta = s.next().toUpperCase().charAt(0);
+//        while( respuesta!='A'&&respuesta!='B'&&respuesta!='C'&&respuesta!='D'
+//                && respuesta!='E')
+//        {
+//            System.out.println("Ingrese la opcion de lo que desea: ");
+//            System.out.println("A. Suma");
+//            System.out.println("B. Resta");
+//            System.out.println("C. Multiplicacion");
+//            System.out.println("D. Division");
+//            System.out.println("E. Salir");
+//            respuesta = s.next().toUpperCase().charAt(0);
+//        }
+//        int a=0,b=0;
+//        if(respuesta!='E')
+//        {
+//            System.out.println("Ingrese un numero:");
+//            a = s.nextInt();
+//        System.out.println("Ingrese un numero:");
+//            b = s.nextInt();
+//        }
+//        switch(respuesta)
+//        {
+//            case 'A':
+//                System.out.println("La respuesta de la suma que solicito es:");
+//                System.out.print(Ejercicio1.suma(a, b));
+//                break;
+//                
+//            case 'B':
+//                System.out.println("La respuesta de la resta que solicito es:");
+//                System.out.print(Ejercicio1.resta(a, b));
+//                break;
+//            case 'C':
+//                System.out.println("La respuesta de la multiplicacion que solicito es:");
+//                System.out.print(Ejercicio1.multiplicacion(a, b));
+//                break;
+//            case 'D':
+//                System.out.println("La respuesta de la division que solicito es:");
+//                System.out.print(Ejercicio1.division(a, b));
+//                break;
+//            default:
+//                System.out.print("Lamento no haber sido de ayuda :(\n");
+//                break;
+//        }
+     Ejercicio2.juego();
     }
     
 }
